4816a52a698ff381e3263c26f6cf7fe2981166d3
Sophia Davis <scdgrapefruit@gmail.com>
2013-11-12 13:08:11 -0600

    improve predictor and scanner

--- parser.py
+++ parser.py
@@ -23,33 +23,38 @@ def main():
     terminals = {}
     
     for line in f:
-#         if line:
-#         print line
-        if line[0] == '#':
-            continue
-        rule = line.split('->')
-        alpha = rule[0].strip(' ')
-        beta = rule[1].strip(' ').strip('\n')
-        if len(beta.split(' ')) > 1:
-            if alpha in branch_rules.keys():
-                branch_rules[alpha].append(beta.split(' '))
+        print line
+        if line.strip('\n'):
+    #         if line:
+    #         print line
+            if line[0] == '#':
+                continue
+            rule = line.split('->')
+            alpha = rule[0].strip(' ')
+            beta = rule[1].strip(' ').strip('\n')
+            if len(beta.split(' ')) > 1:
+                if alpha in branch_rules.keys():
+                    branch_rules[alpha].append(beta.split(' '))
+                else:
+                    branch_rules[alpha] = [beta.split(' ')]
+            elif beta[0].isupper(	):
+                if alpha in pos_rules.keys():
+                    pos_rules[alpha].append(beta)
+                else:
+                    pos_rules[alpha] = [beta]
             else:
-                branch_rules[alpha] = [beta.split(' ')]
-        elif beta.isupper(	):
-            if alpha in pos_rules.keys():
-                pos_rules[alpha].append(beta)
-            else:
-                pos_rules[alpha] = [beta]
-        else:
-            if alpha in terminals.keys():
-                terminals[alpha].append(beta)
-            else:
-                terminals[alpha] = [beta]
+                if alpha in terminals.keys():
+                    terminals[alpha].append(beta)
+                else:
+                    terminals[alpha] = [beta]
     f.close()
     print
     print
+    print 'branch rules: '
     print branch_rules
+    print 'pos rules: '
     print pos_rules
+    print 'terminals: '
     print terminals        
     
     grammar = [branch_rules, pos_rules, terminals]
@@ -69,7 +74,7 @@ def earley(grammar, sentence):
     chart[0] = [['gamma', '.', ['S'], 0, 0]]
     
 #     for i in range(0, n_bins):
-    for i in range(0, n_bins):
+    for i in range(0, 1):
         
         print
         print
@@ -84,6 +89,11 @@ def earley(grammar, sentence):
             chart = scanner(grammar, chart, i, rule, sentence)
             chart = completer(chart, i, rule)
 
+def add_rule(rule, chart_bin):
+    if rule not in chart_bin:
+        print 'adding rule: ', rule
+        chart_bin.append(rule)
+    return chart_bin
 
 def predictor(grammar, chart, index, rule):
     print "in predictor: "
@@ -101,19 +111,16 @@ def predictor(grammar, chart, index, rule):
     if rule[dot + 1][0] in branch_rules.keys():
         print '      its in branch rules!'
         for beta in branch_rules[rule[dot + 1][0]]:
-            print 'appending ', beta
-            chart[index].append([rule[dot + 1][0], '.', beta, index, index])
+            chart[index] = add_rule([rule[dot + 1][0], '.', beta, index, index], chart[index])
+            # this is already a list so leave the brackets off beta!
+
     if rule[dot + 1][0] in pos_rules.keys():
         print '      its in pos rules!'
         for beta in pos_rules[rule[dot + 1][0]]:
-            print 'appending ', [beta]
-            print 'rule', rule[dot + 1][0]
-            print 'index', index
-            print [rule[dot + 1][0], '.', [beta], index, index]
-            print chart[index]
-            chart[index].append([rule[dot + 1][0], '.', [beta], index, index])
+            chart[index] = add_rule([rule[dot + 1][0], '.', [beta], index, index], chart[index])
+
     print "predictor completed: "
-    print chart[index]
+    print 'index', index, chart[index]
     print
     return chart
     
@@ -127,29 +134,29 @@ def scanner(grammar, chart, index, rule, sentence):
     if isinstance(rule[dot + 1], int ):
         return chart
         
-    print rule
     print "looking at: ", rule[dot + 1][0]
     
-    if rule[dot + 1][0] in pos_rules.keys():
-        print '      its in pos rules!'
-        for beta in pos_rules[rule[dot + 1][0]]:# + terminals.keys():
-            print "looking at: ", beta
-            if beta in terminals.keys():
-                print '      its in terminals!'
-                print "looking at the next word: ", sentence[index]
-                if sentence[index] in terminals[beta]:
-                    print " ...matching... "
-                    chart[index + 1].append([beta, [sentence[index]], '.', index, (index + 1)])
+#     if rule[dot + 1][0] in pos_rules.keys():
     if rule[dot + 1][0] in terminals.keys():
         print '      its in terminals!'
+#         print '      its in pos rules!'
+#         for beta in pos_rules[rule[dot + 1][0]]:# + terminals.keys():
+#         for beta in terminals[rule[dot + 1][0]]:    
         print "looking at the next word: ", sentence[index]
         if sentence[index] in terminals[rule[dot + 1][0]]:
             print " ...matching... "
-            chart[index + 1].append([rule[dot + 1][0], [sentence[index]], '.', index, (index + 1)])
-        
+            chart[index + 1] = add_rule([rule[dot + 1][0], [sentence[index]], '.', index, (index + 1)], chart[index + 1])
+
+#     if rule[dot + 1][0] in terminals.keys():
+#         print '      its in terminals!'
+#         print "looking at the next word: ", sentence[index]
+#         if sentence[index] in terminals[rule[dot + 1][0]]:
+#             print " ...matching... "
+# #             chart[index + 1].append([rule[dot + 1][0], [sentence[index]], '.', index, (index + 1)])
+#             chart[index + 1] = add_rule([rule[dot + 1][0], [sentence[index]], '.', index, (index + 1)], chart[index + 1])
     print "scanner completed: "
-    print chart[index]
-    print chart[index + 1]
+    print 'index', index, chart[index]
+    print 'index', index + 1, chart[index + 1]
     print
     return chart
 
@@ -157,19 +164,20 @@ def completer(chart, index, rule):
     print "in completer: "
     
     dot = rule.index('.')
-    print rule
+#     print rule
     
     if isinstance(rule[dot + 1], int ):
         print 'end of rule'
         completed = rule[0]
         print "looking for: ", completed
-        for i in range(0, index):
-            for rule in chart[i]:
-                if [completed] in rule:
-                    at = rule.index([completed])
-                    if rule[at - 1] == '.':
-                        start = rule[3]
-                        chart[index].append(rule[0], )         
+        print '...wip...'
+#         for i in range(0, index):
+#             for rule in chart[i]:
+#                 if [completed] in rule:
+#                     at = rule.index([completed])
+#                     if rule[at - 1] == '.':
+#                         start = rule[3]
+#                         chart[index].append(rule[0], )         
     return chart
     
 if __name__ == '__main__':

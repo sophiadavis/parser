8b87536e8b69195e55ee479ab51e3fcb77b0645d
Sophia Davis <scdgrapefruit@gmail.com>
2013-11-15 18:32:46 -0600

    really submitting

--- parser.py
+++ parser.py
@@ -6,7 +6,7 @@ This program takes a text-file containing a grammar and a sentence (in lower cas
 as commandline arguments, and performs the Earley algorithm to find 
 all possible parses of the sentence given that grammar.
 This implementation assumes the grammar is in Chomsky Normal Form 
-(specifically that grammar rules output no more than two non-terminals) 
+(specifically, that grammar rules output no more than two non-terminals) 
 to format the correct parses.
 """
 import sys
